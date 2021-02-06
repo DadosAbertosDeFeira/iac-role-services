32a72a2b725e86269507cd915a9e36d7a6bcc5bf
Rafael Gomes <linux.rafa@gmail.com>
2021-02-06 19:29:09 -0300

    Começando os testes

---
+++ molecule/default/tests/test_service.py
@@ -0,0 +1,22 @@
+import os
+ 
+import testinfra.utils.ansible_runner
+ 
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+ 
+ 
+def test_if_container_is_running(host):
+    container = host.docker("tika-server")
+ 
+    assert container.is_running
+
+def test_if_container_is_listening_on_container(host):
+    cmd = host.run("docker run --network dokku_services ubuntu sh -c 'apt-get update && apt-get install curl -y && curl tika-server:9998'")
+ 
+    assert cmd.rc == 0
+
+def test_if_container_is_listening_on_host(host):
+    container = host.socket("tcp://0.0.0.0:9998")
+ 
+    assert container.is_listening == False
